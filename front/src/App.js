99695ed992ed7c49a29b07a986261da9ca84493e
NaLeum <wognsdl@likelion.org>
2021-06-17 12:23:49 +0900

    feat: routing 설정

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,20 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Wapper from "./components/common/Wapper";
 import GlobalStyles from "./libs/GlobalStyles";
+import MainPage from "./pages/MainPage";
 
 function App() {
   return (
     <>
       <GlobalStyles/>
-      <div className="App">
-
-      </div>
+      <Wapper>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={MainPage} />
+            <Route path="/*">404 Not Found</Route>
+          </Switch>
+        </Router>
+      </Wapper>
     </>
   );
 }
